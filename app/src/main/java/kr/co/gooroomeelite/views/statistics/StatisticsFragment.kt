9e7618b2fab8f5a66acbcd438f9d675370e66c65
eunyeong1328 <eunyeong1328@daum.net>
2021-06-10 16:09:00 +0900

    add permissions

--- app/src/main/java/kr/co/gooroomeelite/views/statistics/StatisticsFragment.kt
+++ app/src/main/java/kr/co/gooroomeelite/views/statistics/StatisticsFragment.kt
@@ -1,11 +1,21 @@
 package kr.co.gooroomeelite.views.statistics
 
+import android.Manifest
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.material.tabs.TabLayout
+import com.gun0912.tedpermission.PermissionListener
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
 import kr.co.gooroomeelite.R
 
 class StatisticsFragment : Fragment() {
@@ -48,7 +58,54 @@ class StatisticsFragment : Fragment() {
             }
 
         })
+        
+        val shareButton : Button = pageView.findViewById(R.id.share_button)
+        shareButton.setOnClickListener{
+            requestPermission()
+        }
 
         return pageView
     }
+
+    private fun requestPermission(): Boolean{
+        var permissions = false
+        Dexter.withContext(this.activity)
+            .withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            .withListener(object : PermissionListener,
+                com.karumi.dexter.listener.single.PermissionListener {
+                override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                    permissions = true      //p0=response(응답)
+                }
+
+                override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+                    permissions = false
+                    showError()
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    p0: PermissionRequest?,
+                    p1: PermissionToken?
+                ) {
+                    p1!!.continuePermissionRequest()
+                }
+
+                override fun onPermissionGranted() {
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                }
+
+            })
+            .check()
+        return permissions
+    }
+
+    private fun showError(){
+        Toast.makeText(
+            this.requireContext(),
+            "카메라/앨범의 권한을 허용해주세요",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
 }
+
