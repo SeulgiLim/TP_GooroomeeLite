bbbcebe948c89130c42a717451af1304f05f5714
eunyeong1328 <eunyeong1328@daum.net>
2021-06-11 17:20:35 +0900

    add SurfaceViewHoler callback method

--- app/src/main/java/kr/co/gooroomeelite/views/statistics/share/ShareActivity.kt
+++ app/src/main/java/kr/co/gooroomeelite/views/statistics/share/ShareActivity.kt
@@ -5,7 +5,9 @@ import android.hardware.Sensor
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.SurfaceHolder
+import android.view.SurfaceView
 import android.view.WindowManager
 import kr.co.gooroomeelite.R
 
@@ -15,6 +17,10 @@ class ShareActivity : AppCompatActivity() {
     private lateinit var mAccelerometer: Sensor //가속도계
     private lateinit var mMagnetometer: Sensor  //자력계
     private lateinit var mSensorManager: SensorManager //센서관리자
+
+    //스마트폰 크기 호환성
+    private var mHeight: Int = 0
+    private var mWidth:Int = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +39,7 @@ class ShareActivity : AppCompatActivity() {
         setContentView(R.layout.activity_share)
 
         initSensor()
+        initView()
     }
 
     private fun initSensor(){
@@ -40,5 +47,37 @@ class ShareActivity : AppCompatActivity() {
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) //가속도계
         mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) //자력계
 
+    }
+
+    private fun initView(){
+        //스마트폰 크기 호환성 확인!! 없어도 되는 코드
+        with(DisplayMetrics()){
+            windowManager.defaultDisplay.getMetrics(this)
+            mHeight = heightPixels
+            mWidth = widthPixels
+        }
+
+        //viewBinding import가 안될 때
+        val surfaceView :SurfaceView = findViewById(R.id.surfaceView)
+        mSurfaceViewHolder = surfaceView.holder
+        mSurfaceViewHolder.addCallback(object: SurfaceHolder.Callback{
+            override fun surfaceCreated(holder: SurfaceHolder) {
+                TODO("Not yet implemented")
+            }
+
+            override fun surfaceChanged(
+                holder: SurfaceHolder,
+                format: Int,
+                width: Int,
+                height: Int
+            ) {
+                TODO("Not yet implemented")
+            }
+
+            override fun surfaceDestroyed(holder: SurfaceHolder) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
