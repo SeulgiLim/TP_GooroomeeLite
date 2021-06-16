182ecb356ec3d0259324fca2d42c13b6e5dfcfa4
eunyeong1328 <eunyeong1328@daum.net>
2021-06-16 14:46:12 +0900

    add gooroomeeliteAplication

---
+++ app/src/main/java/kr/co/gooroomeelite/views/statistics/share/gooroomeeliteApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.gooroomeelite.views.statistics.share
+
+import android.app.Application
+import androidx.camera.camera2.Camera2Config
+import androidx.camera.core.CameraXConfig
+
+class gooroomeeliteApplication: Application(),CameraXConfig.Provider {
+
+    //
+    override fun getCameraXConfig(): CameraXConfig = Camera2Config.defaultConfig()
+}
