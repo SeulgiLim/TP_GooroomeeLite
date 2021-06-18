f4958f1fbd3122d0560133c7b475faa5810c17b8
SGKwon95 <ssunder7@naver.com>
2021-06-18 17:33:49 +0900

    feat : splash 화면 추가

--- app/src/main/java/kr/co/gooroomeelite/views/common/SplashActivity.kt
+++ app/src/main/java/kr/co/gooroomeelite/views/common/SplashActivity.kt
@@ -4,9 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
-import com.firebase.ui.auth.AuthUI
 import kr.co.gooroomeelite.databinding.ActivitySplashBinding
 import kr.co.gooroomeelite.utils.LoginUtils.Companion.isLogin
 import kr.co.gooroomeelite.utils.RC_SIGN_IN
@@ -26,7 +25,6 @@ class SplashActivity : AppCompatActivity() {
 
         handler.postDelayed({
             startProcess()
-//            finish()
         }, 1000L)
     }
 
@@ -42,28 +40,30 @@ class SplashActivity : AppCompatActivity() {
 
     private fun signIn() {
         startActivityForResult(
-            AuthUI.getInstance().createSignInIntentBuilder()
-                .setTheme(AuthUI.getDefaultTheme())
-                .setLogo(AuthUI.NO_LOGO)
-                .setAvailableProviders(listOf(AuthUI.IdpConfig.GoogleBuilder().build(), AuthUI.IdpConfig.EmailBuilder().build()))
-//                .setTosAndPrivacyPolicyUrls("https://naver.com", "https://google.com")
-//                .setIsSmartLockEnabled(true)
-                .build(),
+            Intent(this, LoginActivity::class.java),
             RC_SIGN_IN
         )
+        finish()
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == RC_SIGN_IN) {
-            if (resultCode == RESULT_OK) {
-                val intent = Intent(this, MainActivity::class.java)
-                intent.putExtras(data!!)
-                startActivity(intent)
-                finish()
-            } else {
-                signIn()
-            }
-        }
-    }
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if (requestCode == RC_SIGN_IN) {
+//            if (resultCode == RESULT_OK) {
+//                val intent = Intent(this, MainActivity::class.java)
+//                intent.putExtras(data!!)
+//                startActivity(intent)
+//                finish()
+//            } else {
+//                AlertDialog.Builder(this)
+//                    .setMessage("로그인하지 않으면 어플을 사용할 수 없습니다.\n종료하시겠습니까?")
+//                    .setPositiveButton("예") { _, _ ->
+//                        finish()
+//                    }
+//                    .setNegativeButton("아니오") { _, _ ->
+//                        signIn()
+//                    }.show()
+//            }
+//        }
+//    }
 }
