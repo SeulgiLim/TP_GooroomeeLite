5bb833e2a75ebc3d1a63d4bb57fa51395a59c352
SGKwon95 <ssunder7@naver.com>
2021-06-23 13:41:08 +0900

    fix : 필드 default 값 추가, prev/next document Id field 값 추가

--- app/src/main/java/kr/co/gooroomeelite/entity/Subject.kt
+++ app/src/main/java/kr/co/gooroomeelite/entity/Subject.kt
@@ -5,12 +5,14 @@ import java.io.Serializable
 import java.util.*
 
 data class Subject(
-    val uid: String?,
-    val name: String,
-    val color: String,
-    val studytime: Int,
+    val uid: String? = null,
+    val name: String? = null,
+    val color: String? = null,
+    val studytime: Int = -1,
+    var prevDocumentId: String? = null,
+    var nextDocumentId: String? = null,
     @ServerTimestamp
-    val timestamp: Date?
+    val timestamp: Date? = null
 ) : Serializable {
-    constructor(uid:String?, name:String, color:String) : this(uid, name, color, 0, Date())
+    constructor(uid:String?, name:String, color:String) : this(uid, name, color, 0, null, null, Date())
 }
