eff02d7ec850816ad66337a672b4dcf89f4a9464
eunyeong1328 <72347107+eunyeong1328@users.noreply.github.com>
2021-07-22 22:40:11 +0900

    add firebase data

--- app/src/main/java/kr/co/gooroomeelite/entity/Subject.kt
+++ app/src/main/java/kr/co/gooroomeelite/entity/Subject.kt
@@ -9,11 +9,12 @@ data class Subject(
     var name: String? = null,
     var color: String? = null,
     var studytime: Int = 0,
+    var studytimeCopy : Int = 0,
     var prevDocumentId: String? = null,
     var nextDocumentId: String? = null,
 
     @ServerTimestamp
     val timestamp: Date? = null,
 ) : Serializable {
-    constructor(uid:String?, name:String, color:String) : this(uid, name, color, 0, null, null, Date())
+    constructor(uid:String?, name:String, color:String) : this(uid, name, color, 0, 0,null, null, Date())
 }
