18ff48edca57e8b61bc0db000ba717fb1a1d6e04
Gnoss-song <silmxmail@naver.com>
2021-07-27 13:10:42 +0900

    stopwatch fix

--- app/src/main/java/kr/co/gooroomeelite/views/home/StudyEndActivity.kt
+++ app/src/main/java/kr/co/gooroomeelite/views/home/StudyEndActivity.kt
@@ -3,6 +3,7 @@ package kr.co.gooroomeelite.views.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_study_end.*
 import kotlinx.coroutines.flow.callbackFlow
@@ -92,4 +93,5 @@ class StudyEndActivity : AppCompatActivity() {
             in 10..59 -> binding.minute.text = minute.toString()
         }
     }
+
 }
